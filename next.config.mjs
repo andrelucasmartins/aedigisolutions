d87e60330b7c2a0406043e75cd2a58f8da75a378
andrelucasmartins <andrelucasmartins@hotmail.com>
2024-02-27 16:35:47 -0300

    feat: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },  
+};
 
 export default nextConfig;
