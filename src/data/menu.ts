746ce78176648b130059e308858e49edbad2c363
andrelucasmartins <andrelucasmartins@hotmail.com>
2024-03-11 17:11:24 -0300

    feat: data menu items

---
+++ src/data/menu.ts
@@ -0,0 +1,27 @@
+export const MENU = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Sobre",
+    url: "/about-us",
+  },
+  {
+    id: 3,
+    title: "Projetos",
+    url: "/projects",
+  },
+  {
+    id: 4,
+    title: "Github",
+    url: "https://github.com/andrelucasmartins",
+  },
+  {
+    id: 6,
+    title: "Contato",
+    url: "/contato",
+  },
+]
