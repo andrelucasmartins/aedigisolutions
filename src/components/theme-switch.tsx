1cf512b4bbdc6543316b6f8ff3c64089decb08f6
andrelucasmartins <andrelucasmartins@hotmail.com>
2024-03-12 18:56:46 -0300

    feat: theme

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,24 +1,24 @@
 'use client'
 
-import { useTheme } from "next-themes"
-import { useEffect, useState } from "react"
-import { FiMoon, FiSun } from "react-icons/fi"
+import { useTheme } from "next-themes";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 export default function ThemeSwitch() {
-  const [mounted, setMounted] = useState(false)
   const { setTheme, resolvedTheme } = useTheme()
 
-  useEffect(() => setMounted(true), [])
-
-  if (!mounted) return (
-    <div>teste</div>
-  )
-
   if(resolvedTheme === 'dark') {
-    return <FiSun onClick={() => setTheme('light')}/>
+    return (
+      <div>
+        <FiSun onClick={() => setTheme("light")} />
+      </div>
+    );
   }
 
   if(resolvedTheme === 'light') {
-    return <FiMoon onClick={() => setTheme('dark')}/>
+    return (
+      <div>
+        <FiMoon onClick={() => setTheme("dark")} />
+      </div>
+    );
   }
 }
